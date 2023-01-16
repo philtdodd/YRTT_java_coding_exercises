d9e8e588d01094c73bf737c05dcb5b2b7e148d8d
Phil Dodd <phil.dodd@cinnamonapps.co.uk>
2023-01-16 19:12:32 +0000

    Completed Excercise001.java

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,26 +5,40 @@ import java.util.List;
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        return firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase();
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        int vatPence = ((int)(originalPrice * vatRate));
+        return originalPrice + vatPence / 100.0;
     }
 
     public String reverse(String sentence) {
         // Add your code here
-        return "";
+        StringBuilder returnValue = new StringBuilder();
+
+        for (int i = sentence.length() - 1; i >= 0; i--)
+        {
+            returnValue.append(sentence.charAt(i));
+        }
+        return returnValue.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
-        return 0;
+        int returnValue = 0;
+        for (User user : users)
+        {
+            if (user.getType().compareTo("Linux") == 0)
+                returnValue++;
+        }
+
+        return returnValue;
     }
 }
